991a833ee2d051b8340e1d1ba3626806a5275b60
Artem Shestopalov <tema.rastaclaus@gmail.com>
2015-04-01 12:08:11 +0300

    ex17 fixed

--- ch4/ex/src/ex17.cpp
+++ ch4/ex/src/ex17.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define F(x,t) \
   t* array_##t = new t[x]; \
   for (int i=0; i < x; ++i) array_##t[i] =  i; \
-  cout << "*array_" #t << "[" #x "],\t" <<  array_##t << endl; \
+  cout << "*array_" #t << "[" #x "],\t" <<  (void*)array_##t << endl; \
 
 #define D(t)\
   delete [] array_##t;
@@ -14,20 +14,6 @@ int main() {
   F(100, int);
   F(100, long);
   F(100, double);
-
-  cout << array_char << endl;
-  cout << array_int << endl;
-  cout << array_long << endl;
-  cout << array_double << endl;
-
-  cout << (long)array_char << endl;
-  cout << (long)array_int << endl;
-  cout << (long)array_long << endl;
-  cout << (long)array_double << endl;
-  /*for (int i = 0; i < 100; ++i) {
-    cout << (int)array_char[i] << ", " << array_int[i]+100 << ", " 
-      << array_long[i] + 1000 << ", " << (array_double[i] + 10000)/3 << endl;
-  }*/
 
   D(char);
   D(int);
