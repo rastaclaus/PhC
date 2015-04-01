51ba1391fffff7506a6c2625daed430ad7cbcb83
Artem Shestopalov <tema.rastaclaus@gmail.com>
2015-04-01 13:11:41 +0300

    ex18 added

---
+++ ch4/ex/src/ex18.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+char* c_read(char* arr, int arr_size) {
+  char * result = new char [arr_size+1];
+  for (int i=0; i <= arr_size; ++i) {
+    result[i] = arr[i];
+  }
+  return result;
+}
+
+int main() {
+  char* hehe = "jopa";
+  char* res1 = c_read(hehe, 4);
+  cout << hehe << endl;
+  cout << res1 << ", it's all, folks!"<< endl;
+  char* res2 = c_read(res1, 4);
+  cout << res2 << ", it's all, folks!"<< endl;
+  cout << static_cast<void*>(res1) << ", " << static_cast<void*>(res2) << endl;
+  delete [] res1;
+  delete [] res2;
+}
