79068167bb04336954a7e6099a2df224b214c592
Artem Shestopalov <tema.rastaclaus@gmail.com>
2015-04-03 18:35:19 +0300

    ex22 done

--- ch4/ex/src/ex22.cpp
+++ ch4/ex/src/ex22.cpp
@@ -7,11 +7,43 @@
 
 using namespace std;
 
-int main() {
-  ifstream in("../makefile");
-  assure(in, "../makefile");
-  string l;
-  while (getline(in, l)){
-    cout << l << endl;
+Stash* read_five(ifstream& in){
+  string line;
+  Stash* stringStash = new Stash;
+  const int bufsize = 80;
+  stringStash->initialize(sizeof(char) * bufsize);
+  int count  = 0;
+  while (getline(in,line) && count++ < 5){
+    stringStash->add(line.c_str());
   }
+  if (count == 0)
+    return 0;
+  return stringStash;
+}
+
+int main() {
+  ifstream in("makefile");
+  assure(in, "makefile");
+
+  Stack stack;
+  Stash* stash = 0;
+
+  stack.initialize();
+  while ((stash = read_five(in))!=0){
+    stack.push(stash);
+  }
+
+  Stack fl;
+  fl.initialize();
+  while ((stash = (Stash*)stack.pop())!=0){
+    fl.push(stash);
+  }
+  stack.cleanup();
+
+  while ((stash = (Stash*)fl.pop())!=0){
+    for (int i=0; i < stash->count(); ++i)
+      cout << (char*)stash->fetch(i) << endl;
+    stash->cleanup();
+  }
+  fl.cleanup();
 }
